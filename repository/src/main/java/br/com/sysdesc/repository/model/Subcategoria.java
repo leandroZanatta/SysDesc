fe0c38d1493946c11425dd1831cc9e5db63f5c15
leandro zanatta <leandrozanatta@sysmo.com.br>
2019-07-03 21:03:34 -0300

    doc: criado Frm de subcategoria

---
+++ repository/src/main/java/br/com/sysdesc/repository/model/Subcategoria.java
@@ -0,0 +1,36 @@
+package br.com.sysdesc.repository.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_subcategoria")
+@SequenceGenerator(name = "GEN_SUBCATEGORIA", allocationSize = 1, sequenceName = "GEN_SUBCATEGORIA")
+public class Subcategoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GEN_SUBCATEGORIA")
+	@Column(name = "id_subcategoria")
+	private Long idSubcategoria;
+
+	@Column(name = "tx_descricao")
+	private String descricao;
+
+	@ManyToOne
+	@JoinColumn(name = "cd_categoria")
+	private Categoria categoria;
+
+}
