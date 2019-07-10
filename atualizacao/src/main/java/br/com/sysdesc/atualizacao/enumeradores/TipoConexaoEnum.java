b7098165e444bab8bfd4f4f3a2296739f1d73b97
Leandro Zanatta <leandrozanatta@sysmo.com.br>
2019-07-10 18:09:55 -0300

    Conexão H2

--- atualizacao/src/main/java/br/com/sysdesc/atualizacao/enumeradores/TipoConexaoEnum.java
+++ atualizacao/src/main/java/br/com/sysdesc/atualizacao/enumeradores/TipoConexaoEnum.java
@@ -2,57 +2,59 @@ package br.com.sysdesc.atualizacao.enumeradores;
 
 public enum TipoConexaoEnum {
 
-	POSTGRES("org.postgresql.Driver", "jdbc:postgresql://localhost",
-			"SELECT datname FROM pg_database WHERE datistemplate = false", "postgres", 5432),
+    POSTGRES("org.postgresql.Driver", "jdbc:postgresql://localhost", "SELECT datname FROM pg_database WHERE datistemplate = false", "postgres", 5432),
 
-	MYSQL("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "show databases", "", 3306);
+    H2("org.postgresql.Driver", "jdbc:h2:mem:sysdesc;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false;MODE=PostgreSQL;MVCC=true",
+            "SELECT datname FROM pg_database WHERE datistemplate = false", "", 5432),
 
-	private String driver;
+    MYSQL("com.mysql.jdbc.Driver", "jdbc:mysql://localhost", "show databases", "", 3306);
 
-	private String url;
+    private String driver;
 
-	private String database;
+    private String url;
 
-	private String defaultDatabase;
+    private String database;
 
-	private Integer porta;
+    private String defaultDatabase;
 
-	public static String jdbcPassword = "javax.persistence.jdbc.password";
-	public static String jdbcUser = "javax.persistence.jdbc.user";
-	public static String jdbcUrl = "javax.persistence.jdbc.url";
-	public static String jdbcDriver = "javax.persistence.jdbc.driver";
+    private Integer porta;
 
-	private TipoConexaoEnum(String driver, String url, String database, String defaultDatabase, Integer porta) {
-		this.driver = driver;
-		this.url = url;
-		this.porta = porta;
-		this.database = database;
-		this.defaultDatabase = defaultDatabase;
+    public static String jdbcPassword = "javax.persistence.jdbc.password";
+    public static String jdbcUser = "javax.persistence.jdbc.user";
+    public static String jdbcUrl = "javax.persistence.jdbc.url";
+    public static String jdbcDriver = "javax.persistence.jdbc.driver";
 
-	}
+    private TipoConexaoEnum(String driver, String url, String database, String defaultDatabase, Integer porta) {
+        this.driver = driver;
+        this.url = url;
+        this.porta = porta;
+        this.database = database;
+        this.defaultDatabase = defaultDatabase;
 
-	public String getDriver() {
-		return driver;
-	}
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getDriver() {
+        return driver;
+    }
 
-	public Integer getPorta() {
-		return porta;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public String getDatabase() {
-		return database;
-	}
+    public Integer getPorta() {
+        return porta;
+    }
 
-	public String getDefaultDatabase() {
-		return defaultDatabase;
-	}
+    public String getDatabase() {
+        return database;
+    }
 
-	@Override
-	public String toString() {
-		return this.driver;
-	}
+    public String getDefaultDatabase() {
+        return defaultDatabase;
+    }
+
+    @Override
+    public String toString() {
+        return this.driver;
+    }
 }
