cbb00a84e7e036aacae5d9b481de143368539ded
jpgra <jpgra@DESKTOP-SC41GN4>
2019-08-19 21:04:18 -0300

    Criamos a Tela de validação da senha

--- components/src/main/java/br/com/sysdesc/components/ValidarSenha.java
+++ components/src/main/java/br/com/sysdesc/components/ValidarSenha.java
@@ -1,17 +1,28 @@
 package br.com.sysdesc.components;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
+
+import br.com.sysdesc.util.classes.CryptoUtil;
+import br.com.sysdesc.util.classes.StringUtil;
 
 public class ValidarSenha extends JDialog {
 
+	private static final long serialVersionUID = 1L;
+	private JButton btConfirmar;
+	private JButton btCancelar;
 	private JLabel lbSenha;
 	private JLabel lbRepSenha;
 	private JPasswordField txSenha;
 	private JPasswordField txRepSenha;
+	private Boolean ok = Boolean.FALSE;
+	private String senha;
 
 	public ValidarSenha() {
+
 		setModal(true);
 		setUndecorated(true);
 		setSize(320, 200);
@@ -21,23 +32,62 @@ public class ValidarSenha extends JDialog {
 		lbRepSenha = new JLabel("Confirmar Senha:");
 		txSenha = new JPasswordField();
 		txRepSenha = new JPasswordField();
+		btConfirmar = new JButton("Confirmar");
+		btCancelar = new JButton("Cancelar");
 
 		lbSenha.setBounds(10, 10, 100, 25);
 		txSenha.setBounds(10, 35, 300, 25);
 		lbRepSenha.setBounds(10, 70, 100, 25);
 		txRepSenha.setBounds(10, 95, 300, 25);
+		btConfirmar.setBounds(55, 140, 100, 25);
+		btCancelar.setBounds(165, 140, 100, 25);
 
 		getContentPane().setLayout(null);
 
+		btConfirmar.addActionListener((e) -> validarSenha());
+		btCancelar.addActionListener((e) -> cancelar());
 		getContentPane().add(lbSenha);
 		getContentPane().add(txSenha);
 		getContentPane().add(lbRepSenha);
 		getContentPane().add(txRepSenha);
-
+		getContentPane().add(btConfirmar);
+		getContentPane().add(btCancelar);
 	}
 
-	public static void main(String[] args) {
-		new ValidarSenha().setVisible(true);
+	private void cancelar() {
+
+		dispose();
+	}
+
+	private void validarSenha() {
+
+		String senha = StringUtil.arrayToString(txSenha.getPassword());
+
+		if (senha.length() < 3) {
+			JOptionPane.showMessageDialog(null, "A senha deve possuir mais de três caracteres");
+
+			return;
+		}
+
+		if (!senha.equals(StringUtil.arrayToString(txRepSenha.getPassword()))) {
+			JOptionPane.showMessageDialog(null, "Campos diferentes");
+
+			return;
+		}
+
+		ok = Boolean.TRUE;
+
+		this.senha = CryptoUtil.toMD5(senha);
+
+		dispose();
+	}
+
+	public Boolean getOk() {
+		return ok;
+	}
+
+	public String getSenha() {
+		return senha;
 	}
 
 }
--- repository/src/main/java/br/com/sysdesc/repository/dao/UsuarioDAO.java
+++ repository/src/main/java/br/com/sysdesc/repository/dao/UsuarioDAO.java
@@ -2,7 +2,10 @@ package br.com.sysdesc.repository.dao;
 
 import static br.com.sysdesc.repository.model.QUsuario.usuario1;
 
+import com.mysema.query.BooleanBuilder;
+
 import br.com.sysdesc.repository.model.Usuario;
+import br.com.sysdesc.util.classes.LongUtil;
 
 public class UsuarioDAO extends AbstractGenericDAO<Usuario> {
 
@@ -13,6 +16,17 @@ public class UsuarioDAO extends AbstractGenericDAO<Usuario> {
 	public Usuario obterLogin(String usuario, String md5) {
 
 		return from().where(usuario1.usuario.eq(usuario).and(usuario1.senha.eq(md5))).singleResult(usuario1);
+	}
+
+	public boolean verificarUsuarioJaCadastrado(Long idCliente, Long idUsuario) {
+
+		BooleanBuilder booleanBuilder = new BooleanBuilder(usuario1.cliente.idCliente.eq(idCliente));
+
+		if (!LongUtil.isNullOrZero(idUsuario)) {
+			booleanBuilder.and(usuario1.idUsuario.ne(idUsuario));
+		}
+
+		return from().where(booleanBuilder).exists();
 	}
 
 }
