ba81b57290d080159ac28b740cfa0ab028a6e7df
Lucas <Lucas@DESKTOP-RQU239S>
2019-08-20 22:38:16 -0300

    Criado PDV

---
+++ sysdesc-rest/src/main/java/br/com/sysdesc/sysdescrest/SysdescRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysdesc.sysdescrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysdescRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysdescRestApplication.class, args);
+	}
+
+}
