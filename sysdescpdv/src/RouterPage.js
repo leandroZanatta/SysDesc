75dab8fef0a0a773d5a89ebb7f7824320274cfdb
Leandro Zanatta <leandrozanatta@sysmo.com.br>
2019-09-27 12:07:28 -0300

    Adicionado Compilação por electron

--- sysdescpdv/src/RouterPage.js
+++ sysdescpdv/src/RouterPage.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import {
+    Link,
+    Route,
+    HashRouter as Router,
+    Switch
+} from "react-router-dom";
 import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Config from './pages/config/config';
 import Forbiden from './pages/forbiden/forbiden';
 
-
 function RouterPage() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Config} />
-        <Route component={Forbiden} />
-      </Switch>
-    </Router>
-  );
+    return ( 
+	<Router >
+        <Switch >
+			<Route path = "/"  exact component = { Config } /> 
+			<Route component = { Forbiden }/>
+		</Switch> 
+	</Router>
+    );
 }
 
 export default RouterPage;
