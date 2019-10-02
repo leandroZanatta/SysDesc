e1dc6df50d3bb23d551ec7d598b1b5552ac79a2b
Leandro Zanatta <leandrozanatta@sysmo.com.br>
2019-10-02 18:21:32 -0300

    Ajuste repositório

--- repository/src/main/java/br/com/sysdesc/repository/dao/PlanoContasDAO.java
+++ repository/src/main/java/br/com/sysdesc/repository/dao/PlanoContasDAO.java
@@ -1,13 +1,13 @@
 package br.com.sysdesc.repository.dao;
 
-import static br.com.sysdesc.repository.model.QPlanoContas.planoContas;
+import static br.com.sysdesc.repository.model.QPlanoContas.planoContas1;
 
 import br.com.sysdesc.repository.model.PlanoContas;
 
 public class PlanoContasDAO extends AbstractGenericDAO<PlanoContas> {
 
-	public PlanoContasDAO() {
-		super(planoContas, planoContas.idPlanoContas);
-	}
+    public PlanoContasDAO() {
+        super(planoContas1, planoContas1.idPlanoContas);
+    }
 
 }
