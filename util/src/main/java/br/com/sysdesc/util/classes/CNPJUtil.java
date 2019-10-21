c71423eeb08bb6846ccc0e6e1d4674c778e97557
Leandro Zanatta <leandrozanatta@sysmo.com.br>
2019-10-21 12:00:22 -0300

    Removido utilitário

--- util/src/main/java/br/com/sysdesc/util/classes/CNPJUtil.java
+++
@@ -1,9 +0,0 @@
-package br.com.sysdesc.util.classes;
-
-public class CNPJUtil {
-
-	public static Boolean isCNPJValido(String cpf) {
-		return Boolean.TRUE;
-	}
-
-}
--- util/src/main/java/br/com/sysdesc/util/classes/CPFUtil.java
+++
@@ -1,9 +0,0 @@
-package br.com.sysdesc.util.classes;
-
-public class CPFUtil {
-
-	public static Boolean isCPFValido(String cpf) {
-		return Boolean.TRUE;
-	}
-
-}
