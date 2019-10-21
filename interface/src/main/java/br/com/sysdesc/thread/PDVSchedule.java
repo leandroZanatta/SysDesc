2d85060e894067745621ce90bfc2e630ba03e2c0
Leandro <Leandro@DESKTOP-7K69IQ7>
2019-10-20 22:16:27 -0300

    Sincronização de informações

--- interface/src/main/java/br/com/sysdesc/thread/PDVSchedule.java
+++ interface/src/main/java/br/com/sysdesc/thread/PDVSchedule.java
@@ -16,7 +16,7 @@ public class PDVSchedule {
 
 	public void startScheduleAtualizacaoGerenciadores() {
 
-		executorService.scheduleAtFixedRate(new AtualizacaoGerenciadorPDVThread(), 0, 5, TimeUnit.SECONDS);
+		executorService.scheduleAtFixedRate(new AtualizacaoGerenciadorPDVThread(), 0, 20, TimeUnit.SECONDS);
 	}
 
 	public static PDVSchedule getInstance() {
