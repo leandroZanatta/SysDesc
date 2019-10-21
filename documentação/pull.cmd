71d7f61c304294d5da3b492faebbf1e16f7da208
Leandro Zanatta <leandrozanatta@sysmo.com.br>
2019-10-21 12:23:07 -0300

    Doc versões geral

---
+++ documentação/pull.cmd
@@ -0,0 +1,7 @@
+git clone https://github.com/leandroZanatta/sysdesc-pdv.git
+git clone https://github.com/leandroZanatta/sysdesc-http-server.git
+git clone https://github.com/leandroZanatta/sysdesc-dto.git
+git clone https://github.com/leandroZanatta/sysdesc-gerenciador-inicializacao.git
+git clone https://github.com/leandroZanatta/sysdesc-atualizacao.git
+git clone https://github.com/leandroZanatta/SysDesc.git
+git clone https://github.com/leandroZanatta/sysdesc-util.git
